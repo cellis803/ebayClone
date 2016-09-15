16e60b24ec24b0b778182a106020d0ca1f0e5408
cellis803 <cellis803@gmail.com>
2016-09-15 16:06:20 -0400

    tests

--- test/testApp.js
+++ test/testApp.js
@@ -4,35 +4,46 @@ var request = require('supertest');
 var chai = require('chai');
 var assert = chai.assert;
 
+var server;
 describe('eBay Clone App Tests', function () {
 
-    describe('test nothing', function () {
+    before(function () {
+        server = require('../app');
+    });
 
+    describe('test nothing', function () {
         it('should pass', function () {
             assert.isTrue(true);
         })
     });
 
+    describe('loading express', function () {
 
+        it('responds to /', function testSlash(done) {
+            request(server)
+                .get('/')
+                .expect(200, done);
+        });
+
+        it('returns 404 for everything else', function testPath(done) {
+            request(server)
+                .get('/foo/bar')
+                .expect(404, done);
+        });
+    });    
+
+    describe('calling GET /auctions', function () {
+
+        it('returns list of auctions', function testAuctions(done) {
+            request(server)
+                .get('/auctions')
+                .expect(200, done);
+        });
+    });        
+
+    after(function () {
+        server.close();
+    });  
 
 });
 
-describe('loading express', function () {
-  var server;
-  beforeEach(function () {
-    server = require('../app');
-  });
-  afterEach(function () {
-    server.close();
-  });
-  it('responds to /', function testSlash(done) {
-  request(server)
-    .get('/')
-    .expect(200, done);
-  });
-  it('404 everything else', function testPath(done) {
-    request(server)
-      .get('/foo/bar')
-      .expect(404, done);
-  });
-});
